624fa01fed9d546c69363bbb84a07a46a889ab42
RenAlisin23 <150377939+RenAlisin23@users.noreply.github.com>
2025-12-22 12:10:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -177,7 +177,7 @@ if not df.empty:
         return 'background-color: #2b2106; color: #eec234;'
 
     st.dataframe(
-        df_f[["id_amigable","n_rq","n_ticket", "empresa", "modelo", "descripcion","comentarios", "informacion", "fedex_number","fecha_registro"]].style.applymap(color_est, subset=['informacion',"enviado"]),
+        df_f[["id_amigable","n_rq","n_ticket", "empresa", "modelo", "descripcion","comentarios", "informacion", "fedex_number","fecha_registro","enviado"]].style.applymap(color_est, subset=['informacion',"enviado"]),
         use_container_width=True, hide_index=True,
         column_config={
             "id_amigable": "Nº", 
